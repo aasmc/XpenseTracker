075fdac22f8e1fecdcedcf8689cc8dcf8a592f26
Alex Alexeyev <aasmc@yandex.ru>
2023-01-05 01:10:30 +0300

    Move icons to drawable folder

--- app/src/main/java/ru/aasmc/xpensemanager/util/SupportedCurrencies.kt
+++ app/src/main/java/ru/aasmc/xpensemanager/util/SupportedCurrencies.kt
@@ -1,9 +1,19 @@
 package ru.aasmc.xpensemanager.util
 
-val supportedCurrencyToDrawableRes = hashMapOf<String, Int>(
-    "RUB" to 1,
-    "USD" to 2,
-    "EUR" to 3,
-    "GBP" to 4,
-    "CNY" to 5
+import ru.aasmc.xpensemanager.R
+
+val currencyToDarkIcon = hashMapOf<String, Int>(
+    "RUB" to R.drawable.ruble_dark,
+    "USD" to R.drawable.us_dollar_dark,
+    "EUR" to R.drawable.euro_dark,
+    "GBP" to R.drawable.british_pound_dark,
+    "CNY" to R.drawable.yuan_dark
+)
+
+val currencyToLightIcon = hashMapOf<String, Int>(
+    "RUB" to R.drawable.ruble_light,
+    "USD" to R.drawable.us_dollar_light,
+    "EUR" to R.drawable.euro_light,
+    "GBP" to R.drawable.british_pound_light,
+    "CNY" to R.drawable.yuan_light
 )
